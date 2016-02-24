22640b87f142d7c51fb943dfaef46f7ec5b92fef
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2016-02-23 22:45:13 -0500

    Create README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# mta_notifier
+Raspberry pi based notifier for next MTA train arrival.
+
+**just playing around with raspberry pi**
--- notify.py
+++ notify.py
@@ -1,30 +1,36 @@
-import dothat.lcd as lcd
-import dothat.backlight as backlight
-
-lcd.write('test')
-backlight.off()
-
+# Get data from MTA
 import urllib, json, re
+from pprint import pprint
+import datetime
+import math
+import dateutil.parser, pytz
 
-url = "http://mtaapi.herokuapp.com/api?id=114S"
+
+url = "http://api.wheresthefuckingtrain.com/by-id/367"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
-arrivals = data["result"]["arrivals"]
+arrivals = data["data"][0]['S']
+first_two_arrivals = arrivals[:2]
 
-import time
+def minutes_left(arrival):
+	now = datetime.datetime.now(pytz.timezone('US/Eastern'))
+	diff = arrival - now
+	if diff.seconds < 20:
+		diff = 0
+	return int(math.ceil(diff.seconds / 60))
 
-arrival_times = []
+
 for arrival in arrivals:
-	hour = arrival[0:2]
-	if hour == "24":
-		arrival = "00" + arrival[2:] 
-	t = time.strptime(arrival,"%H:%M:%S")
-	arrival_times.append(arrivals)
+	arrival['time'] = dateutil.parser.parse(arrival['time'])
+	arrival['mins_until'] = minutes_left(arrival['time'])
 
-arrival_times = sorted(arrival_times)
-arrival_times = [arrival_time for arrival_time in arrival_times if arrival_time > time.time()]
-print arrival_times
+texts = ["(%s) South Ferry - %d min" % (arrival['route'], arrival['mins_until']) for arrival in arrivals]
+next_train = texts[0]
+print next_train
 
+# Print to DotHat
+# import dothat.lcd as lcd
+# import dothat.backlight as backlight
 
-
-
+# lcd.write('test')
+# backlight.off()
