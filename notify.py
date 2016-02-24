300e5a8e5d6ed3faf87964aace1eebf99d01d8ae
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2016-02-24 01:50:17 -0500

    add loop

--- notify.py
+++ notify.py
@@ -1,16 +1,9 @@
 # Get data from MTA
 import urllib, json, re
 from pprint import pprint
-import datetime
+import datetime, time
 import math
 import dateutil.parser, pytz
-
-
-url = "http://api.wheresthefuckingtrain.com/by-id/367"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-arrivals = data["data"][0]['S']
-first_two_arrivals = arrivals[:2]
 
 def minutes_left(arrival):
 	now = datetime.datetime.now(pytz.timezone('US/Eastern'))
@@ -21,25 +14,49 @@ def minutes_left(arrival):
 		diff = 0
 	return int(math.ceil(diff.seconds / 60))
 
-
-for arrival in arrivals:
-	arrival['time'] = dateutil.parser.parse(arrival['time'])
-	arrival['mins_until'] = minutes_left(arrival['time'])
-
-texts = ["(%s) South Ferry - %d min" % (arrival['route'], arrival['mins_until']) for arrival in arrivals]
-next_train = texts[0]
-print next_train
+def next_arrival():
+	url = "http://api.wheresthefuckingtrain.com/by-id/367"
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	arrivals = data["data"][0]['S']
+	first_two_arrivals = arrivals[:2]
+	for arrival in arrivals:
+		arrival['time'] = dateutil.parser.parse(arrival['time'])
+		arrival['mins_until'] = minutes_left(arrival['time'])
+	texts = ["(%s) South Ferry - %d min" % (arrival['route'], arrival['mins_until']) for arrival in arrivals]
+	next_train = texts[0]
+	return next_train
 
 # Print to DotHat
 import dothat.lcd as lcd
 import dothat.backlight as backlight
-r=100
-g=100
-b=100
-#backlight.single_rgb(0, r, g, b)
-#backlight.single_rgb(2, r, g, b)
-#backlight.single_rgb(5, r, g, b)
-backlight.rgb(r,g,b)
-lcd.clear()
-lcd.write("145 st ")
-lcd.write(next_train)
+
+def tidyup():
+    backlight.off()
+    backlight.set_graph(0)
+    lcd.clear()
+
+def turn_on_backlight():
+	r=100
+	g=100
+	b=100
+	backlight.rgb(r,g,b)
+
+if __name__ == "__main__":
+	turn_on_backlight()
+	text_to_display = next_arrival()
+	lcd.clear()
+	lcd.write("145 st ")
+	lcd.set_cursor_position(0,1)
+	lcd.write(text_to_display)
+
+	while True:
+		if text_to_display != next_arrival():
+			print "new time found"
+			lcd.clear()
+			text_to_display = next_arrival()
+			lcd.write("145 st ")
+			lcd.write(text_to_display)
+		else:
+			print "new time not found"
+		time.sleep(10)
